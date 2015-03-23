9beac70df1d49507782a7d7fc03601981fefbb3f
donaldhuan <hjwdonald@gmail.com>
2015-03-23 20:12:13 +0800

    continue offrtlist

--- crawler.py
+++ crawler.py
@@ -5,6 +5,7 @@ import os
 #------
 from settings import *
 from entity.SearchItemListEntity import SearchItemListEntity
+from entity.ItemOfferListEntity import ItemOfferListEntity 
 
 class Crawler:
 	def __init__(self, words):
@@ -12,6 +13,11 @@ class Crawler:
  
 
 if __name__ == '__main__':
-	searcher = SearchItemListEntity('dsdディスク')
-	searcher.getItems()
-	print searcher.itemlist
+	#searcher = SearchItemListEntity('iphone 6')
+	#searcher.getItems()
+	#print len(searcher.itemlist)
+	#print searcher.itemlist
+	offer = ItemOfferListEntity('B00PGIPZHE')
+	offer.getOffers()
+	print len(offer.offerlist)
+	print offer.offerlist
--- entity/ItemOfferListEntity.py
+++ entity/ItemOfferListEntity.py
@@ -1,10 +1,54 @@
+# -*- coding: utf-8 -*-
+
 import urllib2
 from bs4 import BeautifulSoup as bs
 import os
 #------
 import settings
+import util
 from retry import retry
 
 class ItemOfferListEntity:
 	def __init__(self, asin):
+		self.url = settings.baseUrl + settings.offerPostfix + asin + '/'
+		self.offerlist = []
+
+	@retry(urllib2.URLError, tries=100, delay=0.1, backoff=1)
+	def createBSObj(self, page=1):
+		html = urllib2.urlopen(self.url+'?startIndex='+str(10*(page-1)))
+		charset = util.getCharset(html)
+		data = html.read()
+		html.close()
+		bsObj = bs(data.decode(charset), 'lxml')
+		return bsObj
+
+	def pageProcess(self, bsObj):
+		div_olpOffer = bsObj.select('div.olpOffer')
+		offers = []
+		for offer in div_olpOffer:
+			div_seller = offer.select('div.olpSellerColumn')[0]
+			offers.append(div_seller.select('a')[0].attrs['href'])
+		return offers
+
+	def getPageNumsAndParseFirstPage(self):
+		bsObj = self.createBSObj()
+
+		#get offers from first page
+		self.offerlist = self.offerlist + self.pageProcess(bsObj)
 		
+		#get number of offer list
+		ul_pagination = bsObj.select('ul.a-pagination')
+		if (len(ul_pagination) == 0):
+			return 1
+		else:
+			lis = ul_pagination[0].select('li')
+			return int(lis[-2].text)
+	
+	def getOffersByPage(self, page):
+		bsObj = self.createBSObj(page)
+		return self.pageProcess(bsObj)
+
+	def getOffers(self):
+		pageNums = self.getPageNumsAndParseFirstPage()
+		for i in range(1, pageNums):
+			self.offerlist = self.offerlist + self.getOffersByPage(i+1)
